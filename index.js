038f4d0ff547db4933e12f0848d9a0bfa08df9e1
klhaug <kristian@snougle.no>
2024-11-27 07:07:14 +0100

    saving

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const comments = fetch('https://jsonplaceholder.typicode.com/comments')
-      .then(response => response.json())
-      .then(json => console.log(json))
+
+//KINDS OF DATA ->  /posts /users /comments /photos /todos
+
+const getData = (kind) => {
+         fetch(`https://jsonplaceholder.typicode.com/${kind}`)
+          .then(response => response.json())
+}
+
+const myTodos = getData('todos');
+
+console.log(myTodos);
+
+
+
+// øv på å skrive funksjoner hver dag nå. Få inn forskjellen på expression, declaration, arrow, etc. 
